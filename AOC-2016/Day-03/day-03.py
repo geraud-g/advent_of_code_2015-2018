38acdc5e3c1300b089a740a2942a720542d3519b
Geraud <geraud.garrigoux@gmail.com>
2016-12-03 13:30:38 +0100

    refacto day03

--- AOC-2016/Day-03/day-03.py
+++ AOC-2016/Day-03/day-03.py
@@ -1,18 +1,20 @@
+def count_valid_triangles(input):
+    counter = 0
+    for chunk in triangles:
+        for index in range(0, len(chunk), 3):
+            triangle = sorted(chunk[index:index + 3])
+            counter += (sum(triangle[:2]) > triangle[2])
+    return counter
+
+
 if __name__ == "__main__":
     with open('input.txt') as f:
         triangles = f.readlines()
     triangles = [list(map(int, triangle.split())) for triangle in triangles]
 
-    counter = 0
-    for row in triangles:
-        triangle = sorted(row)
-        counter += (sum(triangle[:2]) > triangle[2])
-    print("Puzzle 1: {}".format(counter))
+    nbr_valid_triangles = count_valid_triangles(triangles)
+    print("Puzzle 1: {}".format(nbr_valid_triangles))
 
-    counter = 0
     triangles = zip(*triangles)  # rotate list
-    for colomn in triangles:
-        for index in range(0, len(colomn), 3):
-            triangle = sorted(colomn[index:index + 3])
-            counter += (sum(triangle[:2]) > triangle[2])
-    print("Puzzle 2: {}".format(counter))
+    nbr_valid_triangles = count_valid_triangles(triangles)
+    print("Puzzle 2: {}".format(nbr_valid_triangles))
