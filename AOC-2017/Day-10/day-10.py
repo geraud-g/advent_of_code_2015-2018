e443e3c8a0610aa688c9867e1b06dd21f874f842
ster <you@example.com>
2017-12-14 22:45:29 +0100

    day 10 part 1

--- AOC-2017/Day-10/day-10.py
+++ AOC-2017/Day-10/day-10.py
@@ -1,16 +1,33 @@
 def get_input() -> [str]:
     with open('input.txt') as f:
         line = f.readline()
-        return [int(x) for x in line.split(', ')]
+        return [int(x) for x in line.split(',')]
 
 
-def solve_puzzle(lengths):
-    print(lengths)
-    return 1
+def reverse_sub_list(circular_list, current_position, length):
+    list_len = len(circular_list)
+
+    for x in range(length // 2):
+        start = (current_position + x) % list_len
+        end = (current_position - x + length - 1) % list_len
+        circular_list[start], circular_list[end] = circular_list[end], circular_list[start]
+    return circular_list
+
+
+def solve_puzzle(lengths: [int]) -> int:
+    circular_list = list(range(256))
+    current_position = 0
+    skip_size = 0
+
+    for length in lengths:
+        reverse_sub_list(circular_list, current_position, length)
+        current_position += (length + skip_size)
+        skip_size += 1
+    return circular_list[0] * circular_list[1]
 
 
 if __name__ == "__main__":
     puzzle_input = get_input()
+
     solution_1 = solve_puzzle(puzzle_input)
-    # print(f'Part 1: {solution_1}')
-    # print(f'Part 2: {solution_2}')
+    print(f'Part 1: {solution_1}')
