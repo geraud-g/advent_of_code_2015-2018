08b4277dffc9d4e6bda7ee30f34e0c1dfe8fa44c
ster <you@example.com>
2017-12-17 17:47:33 +0100

    refacto day-15

--- AOC-2017/Day-15/day-15.py
+++ AOC-2017/Day-15/day-15.py
@@ -1,63 +1,63 @@
-def get_input() -> [str]:
+import re
+
+from typing import Generator
+
+FACTOR_A = 16807
+FACTOR_B = 48271
+MODULO = 2147483647
+
+
+def get_input() -> (int, int):
     with open('input.txt') as f:
         line = f.readline()
-        return [int(x) for x in line.split(', ')]
+        search_a = re.search(r'Generator A starts with (\w+)', line)
+        input_a = int(search_a.group(1))
+
+        line = f.readline()
+        search_b = re.search(r'Generator B starts with (\w+)', line)
+        input_b = int(search_b.group(1))
+    return input_a, input_b
 
 
-def solve_puzzle(generator_a: int, generator_b: int) -> int:
-    a_factor = 16807
-    b_factor = 48271
-    mod = 2147483647
+def solve_puzzle(a_value: int, b_value: int) -> int:
     counter = 0
-    mask = 65535
+    mask = 0xFFFF
+
     for _ in range(40000000):
-        generator_a = (generator_a * a_factor) % mod
-        generator_b = (generator_b * b_factor) % mod
-        if generator_a & mask == generator_b & mask:
+        a_value = (a_value * FACTOR_A) % MODULO
+        b_value = (b_value * FACTOR_B) % MODULO
+        if a_value & mask == b_value & mask:
             counter += 1
     return counter
 
 
-def get_a_value(generator_a):
-    a_factor = 16807
-    mod = 2147483647
+def value_generator(start_value: int, factor: int, multiple_of: int) -> Generator[int]:
     while True:
-        generator_a = (generator_a * a_factor) % mod
-        if generator_a % 4 == 0:
-            yield generator_a
+        start_value = (start_value * factor) % MODULO
+        if start_value % multiple_of == 0:
+            yield start_value
 
 
-def get_b_value(generator_b):
-    b_factor = 48271
-    mod = 2147483647
-
-    while True:
-        generator_b = (generator_b * b_factor) % mod
-        if generator_b % 8 == 0:
-            yield generator_b
-
-
-def solve_puzzle2(generator_a: int, generator_b: int) -> int:
-    mask = 65535
+def solve_puzzle_2(a_start_value: int, b_start_value: int) -> int:
+    mask = 0xFFFF
     counter = 0
-    a_gen = get_a_value(generator_a)
-    b_gen = get_b_value(generator_b)
-    for x in range(5000000):
-        a = next(a_gen)
-        b = next(b_gen)
-        if a & mask == b & mask:
+    a_value_generator = value_generator(a_start_value, FACTOR_A, 4)
+    b_value_generator = value_generator(b_start_value, FACTOR_B, 8)
+
+    for _ in range(5000000):
+        a_value = next(a_value_generator)
+        b_value = next(b_value_generator)
+        if a_value & mask == b_value & mask:
             counter += 1
 
     return counter
 
 
 if __name__ == "__main__":
-    a = 783
-    b = 325
-    # puzzle_input = get_input()
+    a, b = get_input()
 
     solution_1 = solve_puzzle(a, b)
     print(f'Part 1: {solution_1}')
 
-    solution_2 = solve_puzzle2(a, b)
+    solution_2 = solve_puzzle_2(a, b)
     print(f'Part 2: {solution_2}')
